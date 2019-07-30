fc0292211c3c5c414f52fe90167e36e86449110f
Ben Woods <woodsy867@gmail.com>
2019-07-30 16:13:43 +0800

    adding players

--- render.js
+++ render.js
@@ -1,27 +1,37 @@
 cs = {};
-suits = 'DCHS';
+suit = 'DCHS';
 number = '34567890JQKA2';
 cards = {}
+players = []
 function preload(){
-    for (x = 0; x<52; x++){
-        cs[number[floor(x/4)]+suits[x%4]] = (loadImage('cards/'+number[floor(x/4)]+suits[x%4]+'.png'))
+    for (var x = 0; x<52; x++){
+        cs.push(loadImage('cards/'+number[floor(x/4)]+suits[x%4]+'.png'))
     }
 }
 
 function setup(){
     createCanvas(500,500);
-    player = new Player();
-    player.cards = [new Card('JH')]
+    for (var x = 0; x<13; x++){
+        for (var y = 0; y<4; y++){
+            val = number[x]+suit[y]
+            cards[val] = new Card(val,cs[x*4+y])
+        }
+    }
+    for (var x = 0; x<4; x++){
+        players.push(new Player(x))
+    }
+    player[0].cards = ['JH','9S']
+
 }
 
 function draw(){
-    player.display()
+    player[0].display()
 }
-function Card(v){
+function Card(v,s){
     this.value = v;
-
+    this.source = s;
     this.draw = function (x,y) {
-        image(cs[this.value],x,y)
+        image(this.source,x,y)
     }
 }
 
@@ -29,13 +39,14 @@ function Container () {
     this.cards = [];
 }
 
-function Player () {
+function Player (n) {
     Container.call(this);
 
     this.x = 0;
     this.y = 0;
+    this.pn = n;
     this.display = function () {
-        this.cards.forEach(x => x.draw(10,10))
+        this.cards.forEach((x,y) => cards[x].draw(100,100*y))
     }
 }
 
