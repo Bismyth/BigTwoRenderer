0c711eb476feb8badffc349777cea50ae4e07c57
Ben Woods <woodsy867@gmail.com>
2019-08-02 14:00:27 +0800

    big dumb

--- render.js
+++ render.js
@@ -26,7 +26,7 @@ function setup(){
     }
     pile = new Centre();
     players[0].cards = ['0S','AC'];
-    players[1].cards = ['1S','4C'];
+    players[1].cards = ['7S','4C'];
     players[2].cards = ['2S','5C'];
     players[3].cards = ['3S','6C'];
     cent = [(canvsize[0]-cs[0].width)/2,(canvsize[1]-cs[0].height)/2];
