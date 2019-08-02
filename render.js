03d7d0e4f9c52e8e52d256a694d20263d13cbbb4
Ben Woods <woodsy867@gmail.com>
2019-08-02 09:49:18 +0800

    adding moving functionality

--- render.js
+++ render.js
@@ -3,6 +3,8 @@ suit = 'DCHS';
 number = '34567890JQKA2';
 cards = {}
 players = []
+canvsize = [500,500]
+nword = true;
 function preload(){
     for (var x = 0; x<52; x++){
         cs.push(loadImage('cards/'+number[floor(x/4)]+suit[x%4]+'.png'))
@@ -10,7 +12,7 @@ function preload(){
 }
 
 function setup(){
-    createCanvas(500,500);
+    createCanvas(canvsize[0],canvsize[1]);
     for (var x = 0; x<13; x++){
         for (var y = 0; y<4; y++){
             val = number[x]+suit[y]
@@ -21,17 +23,23 @@ function setup(){
         players.push(new Player(x))
     }
     players[0].cards = ['JH','9S']
-
+    pile = new Centre();
+    pile.card = 'JH'
 }
-
+x = 0
+y = 0
 function draw(){
-    players[0].display()
+    if(nword){
+        players[0].display();
+    }
 }
 function Card(v,s){
     this.value = v;
     this.source = s;
+    this.x = 0;
+    this.y = 0;
     this.draw = function (x,y) {
-        image(this.source,x,y)
+        image(this.source,this.x = x,this.y = y)
     }
 }
 
@@ -49,4 +57,22 @@ function Player (n) {
         this.cards.forEach((x,y) => cards[x].draw(100,100*y))
     }
 }
+function Centre(){
+    this.card = '';
+    this.draw = function(){
+        if (this.card.length > 0) {
+            image(cards[this.card],canvsize[0]/2,canvsize[1]/2)
+        }
+    }
+}
 
+// function playcard(loc,card){
+//     this.card
+// }
+function play(player,card){
+    player.hand = player.hand.filter(function(value){return value != card;});
+    actions.push()
+}
+function mover(card,speed){
+    this.xp = 
+}
